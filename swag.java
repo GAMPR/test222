ebd26e0eb978111ddb368b08f188e3d6a23b3f3b
GAMPR <tigran1918@gmail.com>
2022-03-12 19:12:20 -0500

    All things

---
+++ swag.java
@@ -0,0 +1,11 @@
+public class swag{
+
+	public static void main(String[] args){
+
+		int[] Swag = {5,4,3,2,1};	
+		for(int i = 0; i < Swag.length; i++){ 
+
+			System.out.println("Swag " + Swag[i]);
+		}
+	}
+}
